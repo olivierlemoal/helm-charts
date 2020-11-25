0d14e052c3a6894b4882508b347283b540e1d48e
Olivier Le Moal <olivier.lemoal@nbs-system.com>
2020-11-25 15:44:34 +0100

    fix entrypoint for updater

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -4,7 +4,7 @@ mkdir -p /data/feed-icons
 
 if [ "x$MODE" == "xupdater" ]; then
     echo -e "yes\n" | php /var/www/html/update.php --update-schema
-    exec php /var/www/html/update.php --daemon
+    exec php /var/www/html/update_daemon2.php --daemon
 else
     exec apache2ctl -DFOREGROUND
 fi
