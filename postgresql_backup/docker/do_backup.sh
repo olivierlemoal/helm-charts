01a33306e7dfc9375506c1ac03ee345cfbbb1362
Olivier Le Moal <olivierlemoal@users.noreply.github.com>
2022-03-31 12:43:46 +0200

    Fix zstd cmdline

--- postgresql_backup/docker/do_backup.sh
+++ postgresql_backup/docker/do_backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export DUMP_FILE=backup_`date +%Y%m%d_%H%M%S`.pgdump
 PGPASSWORD=$POSTGRES_PASSWORD pg_dump -d $POSTGRES_DB -U $POSTGRES_USER -h $POSTGRES_HOST -f $DUMP_FILE
-zstd -c $COMPRESSION_LEVEL -o $DUMP_FILE.zst --rm $DUMP_FILE
+zstd -c$COMPRESSION_LEVEL -o $DUMP_FILE.zst --rm $DUMP_FILE
 openssl enc -e -aes-256-cbc -pbkdf2 -nosalt -in ${DUMP_FILE}.zst -out ${DUMP_FILE}.zst.enc -pass pass:$DB_BACKUP_PASSWORD
 
 s3cmd put ${DUMP_FILE}.zst.enc s3://$S3_BACKUP_PATH/history/ --host $S3_ENDPOINT  --access_key $S3_ACCESS_KEY --secret_key $S3_SECRET_KEY --host-bucket $S3_ENDPOINT
