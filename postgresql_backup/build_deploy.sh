7878a1b37be32aa0a4cebd902b0a7c32465b18c6
Olivier Le Moal <olivierlemoal@users.noreply.github.com>
2022-04-01 11:06:20 +0200

    Update build_deploy.sh

--- postgresql_backup/build_deploy.sh
+++ postgresql_backup/build_deploy.sh
@@ -1,5 +1,10 @@
-set -oue
-cd docker/
-docker build -t rg.fr-par.scw.cloud/olivierlm/postgresql-backup:$(date +%Y-%m-%d) -t rg.fr-par.scw.cloud/olivierlm/postgresql-backup:latest .
-docker push "rg.fr-par.scw.cloud/olivierlm/postgresql-backup:latest"
-docker push "rg.fr-par.scw.cloud/olivierlm/postgresql-backup:$(date +%Y-%m-%d)"
+set -euo pipefail
+
+IMAGE_NAME="postgresql-backup"
+RELEASE=$(date +%Y-%m-%d)
+
+buildah build -t rg.fr-par.scw.cloud/olivierlm/$IMAGE_NAME:$RELEASE -t rg.fr-par.scw.cloud/olivierlm/$IMAGE_NAME:latest docker/
+buildah push "rg.fr-par.scw.cloud/olivierlm/$IMAGE_NAME:latest"
+buildah push "rg.fr-par.scw.cloud/olivierlm/$IMAGE_NAME:$RELEASE"
+buildah rmi "rg.fr-par.scw.cloud/olivierlm/$IMAGE_NAME:$RELEASE"
+buildah rmi "rg.fr-par.scw.cloud/olivierlm/$IMAGE_NAME:latest"
